50163124d0771860ba870126eb1ccb808f3e4437
MICAEL SANTANA <micael.santana@t-systems.com>
2023-04-16 12:02:52 -0300

    checkout

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+const path = require('path');
+
 module.exports = {
   outputDir: 'dist',
-  publicPath: '/'
-}
+  publicPath: '/',
+  chainWebpack: config => {
+    config
+      .plugin('copy')
+      .tap(([options]) => {
+        options[0].patterns.push({
+          from: path.resolve(__dirname, 'CNAME'),
+          to: path.resolve(__dirname, 'dist')
+        });
+        return [options];
+      });
+  }
+};
