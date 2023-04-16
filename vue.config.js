217dcb54f6f522cab06a42764c4d69f7edf5257f
MICAEL SANTANA <micael.santana@t-systems.com>
2023-04-16 12:05:11 -0300

    deploys

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,19 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   outputDir: 'dist',
   publicPath: '/',
-  chainWebpack: config => {
-    config
-      .plugin('copy')
-      .tap(([options]) => {
-        options[0].patterns.push({
-          from: path.resolve(__dirname, 'CNAME'),
-          to: path.resolve(__dirname, 'dist')
-        });
-        return [options];
-      });
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'CNAME'),
+            to: path.resolve(__dirname, 'dist')
+          }
+        ]
+      })
+    ]
   }
 };
